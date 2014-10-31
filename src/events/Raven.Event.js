8b9b10f79a8f368c8bb8ef843fc2d2149aef1bd2
Colin <colin@tomorrowevening.com>
2014-10-31 11:05:49 -0700

    More events!

--- src/events/Raven.Event.js
+++ src/events/Raven.Event.js
@@ -1,6 +1,44 @@
 var Raven = Raven || {};
 
+/**
+ * Any events
+ * @param {string} type Event type
+ * @param {object} params Event object parameters
+ */
 Raven.Event = function(type, params) {
-  this.type = type;
-  this.params = params;
-}
+    this.type = type;
+    this.params = params;
+    return this;
+};
+
+Raven.Event.ADDED       = "ravenAdded";
+Raven.Event.REMOVED     = "ravenRemoved";
+Raven.Event.ENABLE      = "ravenEnable";
+Raven.Event.DISABLE     = "ravenDisable";
+Raven.Event.UPDATE      = "ravenUpdate";
+Raven.Event.DRAW        = "ravenDraw";
+Raven.Event.RESIZE      = "ravenResize";
+
+/**
+ * All mouse & touch events
+ * @param {string} type Event type
+ * @param {float} x X position
+ * @param {float} y Y position
+ * @param {int} index Touch index
+ * @param {float} pressure Touch pressure
+ */
+Raven.ActionEvent = function(type, x, y, index, pressure) {
+    this.type = type;
+    this.x = x;
+    this.y = y;
+    this.index = index;
+    this.pressure = pressure;
+    return this;
+};
+
+Raven.ActionEvent.DOWN      = "ravenDown";
+Raven.ActionEvent.MOVE      = "ravenMove";
+Raven.ActionEvent.UP        = "ravenUp";
+Raven.ActionEvent.OVER      = "ravenOver";
+Raven.ActionEvent.OUT       = "ravenOut";
+Raven.ActionEvent.SCROLLED  = "ravenScrolled";
