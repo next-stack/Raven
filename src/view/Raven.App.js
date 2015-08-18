9ab59a33a392e0a27f7e938e5deedb516af440bc
Colin Duffy <colin@tomorrowevening.com>
2015-08-17 20:17:55 -0700

    Bug fixes, GL support, easier with external libs

--- src/view/Raven.App.js
+++ src/view/Raven.App.js
@@ -9,71 +9,80 @@ var Raven = Raven || {};
  */
 
 Raven.App = function(params) {
-	var _this = this;
-	// fullscreens the canvas element
-	this.fullscreen		= false;
-	// in case you need to pause the auto-runner
-	this.playing		= true;
-	// start time, in millseconds
-	this.timer          = new Raven.Timer();
-	// Display stage
-	this.stage			= new Raven.Stage();
-	// for event handling
-	this.supportMobile	= false;
-	// View instance
-	this.view			= null;
-	// if the dom element is visible within the page
-	this.viewable		= this.checkViewable();
+    var _this = this;
+    // fullscreens the canvas element
+    this.fullscreen     = false;
+    // in case you need to pause the auto-runner
+    this.playing        = false;
+    // start time, in millseconds
+    this.timer          = new Raven.Timer();
+    // Display stage
+    this.stage          = new Raven.Stage();
+    // for event handling
+    this.supportMobile  = false;
+    // View instance
+    this.view           = null;
+    // if the dom element is visible within the page
+    this.viewable       = this.checkViewable();
+    // if the app should check if it's in view
+    this.checkView      = true;
 
-	if(params !== undefined) {
-		var p = params;
-		this.fullscreen    = p['fullscreen'] !== undefined ? p['fullscreen'] : this.fullscreen;
-		this.supportMobile = p['mobile'] !== undefined ? p['mobile'] : this.supportMobile;
-	}
+    if(params !== undefined) {
+        var p = params;
+        this.fullscreen    = p['fullscreen'] !== undefined ? p['fullscreen'] : this.fullscreen;
+        this.supportMobile = p['mobile'] !== undefined ? p['mobile'] : this.supportMobile;
+    }
 
-	// Non-extendable functions
+    // Non-extendable functions
 
-	/**
-	 * Starts requestAnimationFrame
-	 */
-	this.autoRender = function() {
-		if(_this.playing) window.requestAnimationFrame(_this.autoRender);
-		if(_this.timer.running) {
-			if(Raven.Ani !== undefined) Raven.Ani.update();
-			if(Raven.Springy !== undefined) Raven.Springy.update();
-			_this.updateHandler();
-		}
-	};
+    /**
+     * Starts requestAnimationFrame
+     */
+    this.autoRender = function() {
+        if(_this.playing) window.requestAnimationFrame(_this.autoRender);
+        if(_this.timer.running) {
+            if(Raven.Ani !== undefined) Raven.Ani.update();
+            if(Raven.Springy !== undefined) Raven.Springy.update();
+            _this.updateHandler();
+        }
+    };
 
-	/**
-	 * Updates the application settings, calls 'update', then renders the frame.
-	 */
-	this.updateHandler = function() {
-		if(_this.view && _this.viewable) {
-			_this.stage.update();
-            _this.update();
-            if(_this.checkViewable()) {
+    /**
+     * Updates the application settings, calls 'update', then renders the frame.
+     */
+    this.updateHandler = function() {
+        if(_this.view) {
+            if( _this.checkView && _this.viewable ) {
+                _this.stage.update();
+                _this.update();
+                if(_this.checkViewable()) {
+                    if(_this.view.autoClear) _this.view.clear();
+                    _this.draw();
+                }
+                _this.timer.update();
+            } else if(!_this.checkView) {
+                _this.stage.update();
+                _this.update();
                 if(_this.view.autoClear) _this.view.clear();
                 _this.draw();
+                _this.timer.update();
             }
-            _this.timer.update();
         } else if(_this.view === null) {
-        	_this.stage.update();
             _this.update();
             _this.draw();
             _this.timer.update();
         }
-	};
+    };
 
-	/**
-	 * Forward the event handler to our app
-	 */
-	this.domHandler = function(evt) {
-		_this.evtHandler(evt);
-	};
+    /**
+     * Forward the event handler to our app
+     */
+    this.domHandler = function(evt) {
+        _this.evtHandler(evt);
+    };
 
-	this.timer.restart();
-	return this;
+    this.timer.restart();
+    return this;
 };
 
 Raven.App.prototype = {
@@ -86,43 +95,47 @@ Raven.App.prototype = {
     maxHeight: function() {
         return window.innerHeight;
     },
-	checkViewable: function() {
-		if(this.view !== null && this.view.available()) {
-			var wRect = Raven.Rect(window.scrollX, window.scrollY, this.maxWidth(), this.maxHeight()),
-			aRect = Raven.Rect().mapToDiv(this.view.element),
-			result = aRect.overlap(wRect);
-			return result;
-		}
-		return false;
-	},
+    checkViewable: function() {
+        if(this.view !== null && this.view.available()) {
+            var wRect = Raven.Rect(window.scrollX, window.scrollY, this.maxWidth(), this.maxHeight()),
+            aRect = Raven.Rect().mapToDiv(this.view.element),
+            result = aRect.overlap(wRect);
+            return result;
+        }
+        return false;
+    },
     pause: function() {
+        if(!this.playing) return;
+        this.playing = false;
         this.timer.pause();
     },
     play: function() {
+        if(this.playing) return;
+        this.playing = true;
         this.timer.play();
         this.autoRender();
     },
-	enable:  function() {
-		var stage = this.checkViewable();
+    enable:  function() {
+        var stage = this.checkViewable();
         if(stage) {
             this.enableElement(this.view.element);
         }
-		if(!Raven.isMobile) {
+        if(!Raven.isMobile) {
             Raven.watch(window, Raven.DOM.KEY_DOWN, this.domHandler);
             Raven.watch(window, Raven.DOM.KEY_UP,   this.domHandler);
-		}
-		Raven.watch(window, Raven.DOM.SCROLL, this.domHandler);
-		if(this.fullscreen) Raven.watch(window, Raven.DOM.RESIZE, this.domHandler);
-	},
-	disable: function() {
-		if( this.checkViewable() ) {
+        }
+        Raven.watch(window, Raven.DOM.SCROLL, this.domHandler);
+        if(this.fullscreen) Raven.watch(window, Raven.DOM.RESIZE, this.domHandler);
+    },
+    disable: function() {
+        if( this.checkViewable() ) {
             this.disableElement( this.view.element );
-		}
-		Raven.unwatch(window, Raven.DOM.KEY_DOWN, this.domHandler);
-		Raven.unwatch(window, Raven.DOM.KEY_UP,   this.domHandler);
-		Raven.unwatch(window, Raven.DOM.RESIZE,   this.domHandler);
-		Raven.unwatch(window, Raven.DOM.SCROLL,   this.domHandler);
-	},
+        }
+        Raven.unwatch(window, Raven.DOM.KEY_DOWN, this.domHandler);
+        Raven.unwatch(window, Raven.DOM.KEY_UP,   this.domHandler);
+        Raven.unwatch(window, Raven.DOM.RESIZE,   this.domHandler);
+        Raven.unwatch(window, Raven.DOM.SCROLL,   this.domHandler);
+    },
     enableElement: function(element) {
         if(Raven.isMobile) {
             Raven.watch(element, Raven.DOM.TOUCH_DOWN, this.domHandler);
@@ -142,9 +155,9 @@ Raven.App.prototype = {
         Raven.unwatch(element, Raven.DOM.MOUSE_MOVE, this.domHandler);
         Raven.unwatch(element, Raven.DOM.MOUSE_UP,   this.domHandler);
     },
-	setup:   function(viewElement, width, height, renderer) {
-		if(viewElement !== undefined && renderer !== undefined) {
-			// Setup the view
+    setup:   function(viewElement, width, height, renderer) {
+        if(viewElement !== undefined && renderer !== undefined) {
+            // Setup the view
             if(renderer == Raven.VIEW_WEBGL) {
                 this.view = new Raven.GLView();
             } else {
@@ -153,121 +166,123 @@ Raven.App.prototype = {
 
             this.view.setup(viewElement);
             this.viewable = Raven.viewable(this.view.element);
-			// Resize view
-			if(this.fullscreen) {
-				this.onResize(this.maxWidth(), this.maxHeight());
-			} else {
-				this.onResize(width, height);
-			}
-			// Clear stage
-			// if(this.view.autoClear) this.view.clear();
-			// Draw base
-			//this.draw();
-		}
+            // Resize view
+            if(this.fullscreen) {
+                this.onResize(this.maxWidth(), this.maxHeight());
+            } else {
+                this.onResize(width, height);
+            }
+            // Clear stage
+            // if(this.view.autoClear) this.view.clear();
+            // Draw base
+            //this.draw();
+        }
         // Enable events
         this.enable();
-	},
-	update:  function() {},
-	draw:    function() {
-		if(!this.viewable) return;
-		if(this.view.type == Raven.VIEW_CANVAS) {
-			this.stage.draw(this.view);
+    },
+    update:  function() {},
+    draw:    function() {
+        if(!this.viewable) return;
+        if(this.view.type == Raven.VIEW_CANVAS) {
+            this.stage.draw(this.view);
 
-			var _align = this.view.align;
-			this.view.align = Raven.Align.TOP_LEFT;
-			this.view.setFillB(0);
-			this.view.drawRect(20, 10, 140, 25, true);
-			this.view.setFillB(255);
-			var output = "Time " + this.timer.seconds.toFixed(2);
-			output += " Frame #" + this.timer.frameNum.toString();
-			this.view.drawFont(output, 25, 25);
-			this.view.drawFont(output, 25, this.view.height-25);
-			this.view.align = _align;
-		} else {
-		}
-	},
-	// Handled
-	onKeyDown:   function(key) {},
-	onKeyUp:     function(key) {},
-	onTouchDown: function(id, x, y) {},
-	onTouchMove: function(id, x, y) {},
-	onTouchUp:   function(id, x, y) {},
-	// Handlers
-	onResize:    function(w, h) {
-		if(this.view && this.view.available()) this.view.resize(w, h);
+            this.view.context.font = "10px sans-serif";
+
+            var _align = this.view.align;
+            this.view.align = Raven.Align.TOP_LEFT;
+            this.view.setFillB(0);
+            this.view.drawRect(20, 10, 140, 25, true);
+            this.view.setFillB(255);
+            var output = "Time " + this.timer.seconds.toFixed(2);
+            output += " Frame #" + this.timer.frameNum.toString();
+            this.view.drawFont(output, 25, 25);
+            this.view.drawFont(output, 25, this.view.height-25);
+            this.view.align = _align;
+        } else {
+        }
+    },
+    // Handled
+    onKeyDown:   function(key) {},
+    onKeyUp:     function(key) {},
+    onTouchDown: function(id, x, y) {},
+    onTouchMove: function(id, x, y) {},
+    onTouchUp:   function(id, x, y) {},
+    // Handlers
+    onResize:    function(w, h) {
+        if(this.view && this.view.available()) this.view.resize(w, h);
         this.stage.resize(w, h);
-	},
-	onScroll:    function(evt, x, y) {},
-	evtHandler:  function(evt) {
-		switch(evt.type) {
-			case Raven.DOM.RESIZE:
+    },
+    onScroll:    function(evt, x, y) {},
+    evtHandler:  function(evt) {
+        switch(evt.type) {
+            case Raven.DOM.RESIZE:
                 if(this.view) this.viewable = Raven.viewable( this.view.element );
-				this.onResize(this.maxWidth(), this.maxHeight());
-				Raven.dispatchEvent( new Raven.Event(Raven.Event.RESIZE) );
-			break;
-			case Raven.DOM.KEY_DOWN:
-				this.onKeyDown( Key.getKey(evt.keyCode) );
-			break;
-			case Raven.DOM.KEY_UP:
-				this.onKeyUp( Key.getKey(evt.keyCode) );
-			break;
-			case Raven.DOM.MOUSE_DOWN:
-				this.onTouchDown( 0, evt.x, evt.y );
-				Raven.dispatchEvent( new Raven.ActionEvent(Raven.ActionEvent.DOWN, evt.x, evt.y) );
-			break;
-			case Raven.DOM.MOUSE_MOVE:
-				this.onTouchMove( 0, evt.x, evt.y );
-				Raven.dispatchEvent( new Raven.ActionEvent(Raven.ActionEvent.MOVE, evt.x, evt.y) );
-			break;
-			case Raven.DOM.MOUSE_UP:
-				this.onTouchUp( 0, evt.x, evt.y );
-				Raven.dispatchEvent( new Raven.ActionEvent(Raven.ActionEvent.UP, evt.x, evt.y) );
-			break;
+                this.onResize(this.maxWidth(), this.maxHeight());
+                Raven.dispatchEvent( new Raven.Event(Raven.Event.RESIZE) );
+            break;
+            case Raven.DOM.KEY_DOWN:
+                this.onKeyDown( Key.getKey(evt.keyCode) );
+            break;
+            case Raven.DOM.KEY_UP:
+                this.onKeyUp( Key.getKey(evt.keyCode) );
+            break;
+            case Raven.DOM.MOUSE_DOWN:
+                this.onTouchDown( 0, evt.x, evt.y );
+                Raven.dispatchEvent( new Raven.ActionEvent(Raven.ActionEvent.DOWN, evt.x, evt.y) );
+            break;
+            case Raven.DOM.MOUSE_MOVE:
+                this.onTouchMove( 0, evt.x, evt.y );
+                Raven.dispatchEvent( new Raven.ActionEvent(Raven.ActionEvent.MOVE, evt.x, evt.y) );
+            break;
+            case Raven.DOM.MOUSE_UP:
+                this.onTouchUp( 0, evt.x, evt.y );
+                Raven.dispatchEvent( new Raven.ActionEvent(Raven.ActionEvent.UP, evt.x, evt.y) );
+            break;
             case Raven.DOM.SCROLL:
                 this.viewable = this.viewable ? Raven.viewable( this.view.element ) : false;
                 var x = 0, y = 0;
                 if(this.viewable) {
-                	x = this.view.element.offsetLeft;
-                	y = this.view.element.offsetTop;
+                    x = this.view.element.offsetLeft;
+                    y = this.view.element.offsetTop;
                 } else {
-                	x = window.scrollX;
-                	y = window.scrollY;
+                    x = window.scrollX;
+                    y = window.scrollY;
                 }
                 this.onScroll( evt, x, y );
                 Raven.dispatchEvent( new Raven.ActionEvent(Raven.ActionEvent.SCROLLED, x, y) );
             break;
             case Raven.DOM.TOUCH_DOWN:
-            	var i, x, y, touches = evt.targetTouches, total = touches.length;
-				for(i = 0; i < total; ++i) {
-					x = touches[i].clientX;
-					y = touches[i].clientY;
-					this.onTouchDown( touches[i].identifier, x, y );
-					Raven.dispatchEvent( new Raven.ActionEvent(Raven.ActionEvent.DOWN, x, y, touches[i].identifier) );
-				}
-				touches = null;
+                var i, x, y, touches = evt.targetTouches, total = touches.length;
+                for(i = 0; i < total; ++i) {
+                    x = touches[i].clientX;
+                    y = touches[i].clientY;
+                    this.onTouchDown( touches[i].identifier, x, y );
+                    Raven.dispatchEvent( new Raven.ActionEvent(Raven.ActionEvent.DOWN, x, y, touches[i].identifier) );
+                }
+                touches = null;
             break;
             case Raven.DOM.TOUCH_MOVE:
-            	var i, x, y, touches = evt.targetTouches, total = touches.length;
-				for(i = 0; i < total; ++i) {
-					x = touches[i].clientX;
-					y = touches[i].clientY;
-					this.onTouchMove( touches[i].identifier, touches[i].clientX, touches[i].clientY );
-					Raven.dispatchEvent( new Raven.ActionEvent(Raven.ActionEvent.MOVE, x, y, touches[i].identifier) );
-				}
-				touches = null;
+                var i, x, y, touches = evt.targetTouches, total = touches.length;
+                for(i = 0; i < total; ++i) {
+                    x = touches[i].clientX;
+                    y = touches[i].clientY;
+                    this.onTouchMove( touches[i].identifier, touches[i].clientX, touches[i].clientY );
+                    Raven.dispatchEvent( new Raven.ActionEvent(Raven.ActionEvent.MOVE, x, y, touches[i].identifier) );
+                }
+                touches = null;
             break;
             case Raven.DOM.TOUCH_UP:
-            	var i, x, y, touches = evt.targetTouches, total = touches.length;
-				for(i = 0; i < total; ++i) {
-					x = touches[i].clientX;
-					y = touches[i].clientY;
-					this.onTouchUp( touches[i].identifier, touches[i].clientX, touches[i].clientY );
-					Raven.dispatchEvent( new Raven.ActionEvent(Raven.ActionEvent.UP, x, y, touches[i].identifier) );
-				}
-				touches = null;
+                var i, x, y, touches = evt.targetTouches, total = touches.length;
+                for(i = 0; i < total; ++i) {
+                    x = touches[i].clientX;
+                    y = touches[i].clientY;
+                    this.onTouchUp( touches[i].identifier, touches[i].clientX, touches[i].clientY );
+                    Raven.dispatchEvent( new Raven.ActionEvent(Raven.ActionEvent.UP, x, y, touches[i].identifier) );
+                }
+                touches = null;
             break;
-		}
-	}
+        }
+    }
 };
 
 Raven.App.prototype.constructor = Raven.App;
