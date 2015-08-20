d979f4c69549f15b22bc557086a546df8a5f29a2
Colin Duffy <colin@tomorrowevening.com>
2015-08-19 22:13:22 -0700

    SVG addition

--- src/display/Raven.Shape.js
+++ src/display/Raven.Shape.js
@@ -5,6 +5,7 @@ Raven.Shape = function(params) {
     this.name   = "Raven.Shape_" + Raven.Shape.count.toString();
     this.image  = undefined;
     this.loaded = false;
+    this.svg    = "";
     ++Raven.Shape.count;
     return this;
 };
@@ -25,19 +26,28 @@ Raven.Shape.prototype.render = function(view) {
     return this;
 };
 
-Raven.Shape.prototype.setSVG = function(svgString, onComplete) {
+Raven.Shape.prototype.rerender = function(onComplete) {
+    if(this.svg.length < 1) return this; // needs svg
+    //
     var _this  = this;
     var DOMURL = window.URL || window.webkitURL || window;
-    this.image = new Image();
-    var svg    = new Blob([svgString], {type: 'image/svg+xml;charset=utf-8'});
+    var svg    = new Blob([this.svg], {type: 'image/svg+xml;charset=utf-8'});
     var url    = DOMURL.createObjectURL(svg);
-    this.image.onload = function () {
+    var image  = new Image();
+    image.onload = function () {
       DOMURL.revokeObjectURL(url);
-      _this.size.x = _this.image.width;
-      _this.size.y = _this.image.height;
+      _this.size.x = image.width;
+      _this.size.y = image.height;
+      _this.image  = image;
       _this.loaded = true;
       if(onComplete !== undefined) onComplete(_this);
     }
-    this.image.src = url;
+    image.src = url;
+    return this;
+};
+
+Raven.Shape.prototype.setSVG = function(svgString, onComplete) {
+    this.svg = svgString;
+    this.rerender( onComplete );
     return this;
 };
